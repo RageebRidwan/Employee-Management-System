7bceb0e59acc387a8458521a6a642195a15d3ab9
RageebRidwan <rageeb.cs@gmail.com>
2025-01-26 16:00:05 +0600

    changes added

--- EMS/settings.py
+++ EMS/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['employee-management-system-4thw.onrender.com']
 CSRF_TRUSTED_ORIGINS = [
-'https://employee-management-system-4thw.onrender.com/','https://*.127.0.0.1'  # Use the full URL
+'https://employee-management-system-4thw.onrender.com','https://*.127.0.0.1'  # Use the full URL
 ]
 
 # Application definition
