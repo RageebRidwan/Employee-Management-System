7adc9c493e25fa34219df1503e07681b1b4b0452
RageebRidwan <rageeb.cs@gmail.com>
2025-01-26 15:31:52 +0600

    another change

--- EMS/settings.py
+++ EMS/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h@=f6coz-kk(5lqf5nxy%iz8r_n@m=1mbaj(*0_s82t#9$vovc"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["employee-management-system-lxeg.onrender.com"]
-
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
+CSRF_TRUSTED_ORIGINS = [
+    "https://employee-management-system-lxeg.onrender.com",  # Use the full URL
+]
 
 # Application definition
 
