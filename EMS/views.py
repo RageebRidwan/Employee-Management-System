e435ca5258b278b30aee7c0ddc8180b9a31d9791
RageebRidwan <rageeb.cs@gmail.com>
2025-02-07 07:42:46 +0600

    search functionality added

--- EMS/views.py
+++ EMS/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from user.models import Profile
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
 
 def entry(request):
     return render(request, "entry.html")
 
 
+
 @login_required
 def home(request):
-    data = Profile.objects.all()
+    query = request.GET.get("q")
+    if query:
+        data = Profile.objects.filter(
+            Q(first_name__icontains=query)
+            | Q(last_name__icontains=query)
+            | Q(email__icontains=query)
+        )
+    else:
+        data = Profile.objects.all()
     return render(request, "home.html", {"data": data})
